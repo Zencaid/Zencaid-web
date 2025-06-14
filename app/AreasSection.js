849588c6b92a54aeb41eb7effb856b054f4b206b
Atrojiggambob <mathiasonyebuchi13@gmail.com>
2025-06-14 23:58:03 +0100

    fresh

---
+++ app/AreasSection.js
@@ -0,0 +1,44 @@
+const AreasSection =[
+    {
+        id:1 ,
+        title:'Food & Nutrition',
+        content:'+20 Doctors are avalaible under this department who serve'
+    },
+    {
+        id:2 ,
+        title:'Optamologist',
+        content:'+20 Doctors are avalaible under this department who serve'
+    },
+    {
+        id:3 ,
+        title:'Dental Care',
+       content:'+20 Doctors are avalaible under this department who serve'
+    },
+    {
+        id:4 ,
+        title:'Sugery',
+        content:'+25 Doctors are avalaible under this department who serve'
+    },
+    {
+        id:5 ,
+        title:'Orphopedic',
+       content:'+20 Doctors are avalaible under this department who serve'
+    },
+    {
+        id:6,
+        title:'Neurology',
+        content:'+20 Doctors are avalaible under this department who serve'
+    },
+
+    {
+        id:7,
+        title:'Cardiology',
+        content:'+25 Doctors are avalaible under this department who serve'
+    },
+    {
+        id:8,
+        title:'Medicine & Nephrology',
+        content:'+20 Doctors are avalaible under this department who serve'
+    }
+
+]
---
+++ app/containerinfo.js
@@ -0,0 +1,29 @@
+const containerinfo = [
+    {
+        id:1,
+        title:'Visitor Information',
+        content:'View all information about the visitor and follow all terms and condition',
+        backgroundColor:''
+
+    },
+     {
+        id:2,
+        title:'Find a Doctor',
+        content:'View all information about the visitor and follow all terms and condition',
+        backgroundColor:''
+
+    }, {
+        id:3,
+        title:'Our Location',
+        content:'View all information about the visitor and follow all terms and condition',
+        backgroundColor:''
+
+    },
+     {
+        id:4,
+        title:'Connect with us',
+        content:'View all information about the visitor and follow all terms and condition',
+        backgroundColor:''
+
+    }
+]
